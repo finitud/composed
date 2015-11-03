432e6cd38f5b8e923d25c8a4ef785475243033ad
Alice Wyan <alice@finitud.org>
2015-11-03 16:49:08 +0000

    Implement empty checkout case

--- lib/composed/checkout.rb
+++ lib/composed/checkout.rb
@@ -1,5 +1,20 @@
 module Composed
   class Checkout
+    def initialize(pricing_rules)
+      @pricing_rules = pricing_rules
+      @content ||= {}
+    end
 
+    def scan(item)
+      @content[item] = @content.fetch(item, 0) + 1
+    end
+
+    def price
+      total = 0.0
+      @content.each do |item, quantity|
+        total += @pricing_rules.price(item, quantity)
+      end
+      total
+    end
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,6 +2,7 @@ require 'composed'
 # coding: utf-8
 
 RSpec.describe Composed::Checkout do
+
   context 'should implement the given interface' do
   end
 
@@ -9,21 +10,25 @@ RSpec.describe Composed::Checkout do
     let(:pricing_rules) { nil }
     let(:co) { Composed::Checkout.new(pricing_rules) }
 
-    it 'FR1,SR1,FR1,FR1,CF1' do
+    it 'empty checkout' do
+      expect(co.price).to eq(0.0)
+    end
+
+    xit 'FR1,SR1,FR1,FR1,CF1' do
       %w(FR1 SR1 FR1 FR1 CF1).each do |item|
         co.scan(item)
       end
       expect(co.price).to eq(22.45)
     end
 
-    it 'FR1,FR1' do
+    xit 'FR1,FR1' do
       %w(FR1 FR1).each do |item|
         co.scan(item)
       end
       expect(co.price).to eq(3.11)
     end
 
-    it 'SR1,SR1,FR1,SR1' do
+    xit 'SR1,SR1,FR1,SR1' do
       %w(SR1 SR1 FR1 SR1).each do |item|
         co.scan(item)
       end
